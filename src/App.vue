24e82dae28f61414084b794707f013e720a2dd4c
puremana <puremana@gmail.com>
2021-05-12 10:00:13 +1200

    Change font from Noto Sans to Merienda (more of a foody font)

--- src/App.vue
+++ src/App.vue
@@ -384,10 +384,10 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Noto+Sans:400,700');
+@import url('https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&display=swap');
 
 body {
-  font-family: 'Noto Sans', sans-serif;
+  font-family: 'Merienda', cursive;
   text-align: center;
 
   background: url('/background.jpg');
